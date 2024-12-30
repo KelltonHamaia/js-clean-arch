50e1670eb33b28f827be1308d2d8b0bfd68bab52
kellton_hamaia <hamaia.kell@gmail.com>
2024-12-30 14:30:36 -0300

    test: criar teste retorna Either.left caso as data de retorno seja menor que a de saida

--- src/application/usecases/borrow-a-book.spec.js
+++ src/application/usecases/borrow-a-book.spec.js
@@ -1,3 +1,4 @@
+const { Either } = require("../errors");
 const borrowABookUseCase = require("./borrow-a-book")
 
 
@@ -28,6 +29,21 @@ describe("Borrow a book Use Case", () => {
         expect(loanRepository.findBooksUnderLoanFromUser).toHaveBeenCalledTimes(1);
         expect(loanRepository.findBooksUnderLoanFromUser).toHaveBeenCalledWith({ userId: borrowDTO.userId, bookId: borrowDTO.bookId });
 
+    });
+
+    it("Should return an Either.left if the departure date greater than return date", async () => {
+        const borrowDTO = {
+            userId: "valid_id",
+            bookId: "valid_id",
+            departureDate: new Date("2025-01-10"),
+            returnDate: new Date("2024-12-10")
+        }
+
+        const sut = borrowABookUseCase({ loanRepository });
+        const output = await sut(borrowDTO);
+
+        expect(output.left).toBe(Either.DepartureDateLowerThanReturnDate);
     })
+
 
 })
