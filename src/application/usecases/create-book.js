3f0de469d9302fbf35ce0e6b2dd2a8855452d52b
kellton_hamaia <hamaia.kell@gmail.com>
2024-12-30 01:06:57 -0300

    test: criar teste que retorna Either.left se o ISBN informado já é usado

--- src/application/usecases/create-book.js
+++ src/application/usecases/create-book.js
@@ -9,6 +9,9 @@ module.exports = function createBookUseCase( { bookRepository }) {
         const allParamsCorrectlyProvided = name && quantity && author && genre && isbn;
         if(!allParamsCorrectlyProvided) throw new AppError(AppError.missingParams);
 
+        const isbnAlreadyInUse = await bookRepository.findByIsbn(isbn);
+        if(isbnAlreadyInUse) return Either.Left(Either.FieldAlreadyTaken("isbn"));
+        
         await bookRepository.save({ 
             name, 
             quantity, 
--- src/application/usecases/create-book.spec.js
+++ src/application/usecases/create-book.spec.js
@@ -1,10 +1,11 @@
-const { AppError } = require("../errors");
+const { AppError, Either } = require("../errors");
 const createBookUseCase = require("./create-book");
 
 describe("Create book Use Case", () => { 
 
     const bookRepository = {
-        save: jest.fn()
+        save: jest.fn(),
+        findByIsbn: jest.fn()
     }
 
     it("Should create a book", async () => {
@@ -32,6 +33,25 @@ describe("Create book Use Case", () => {
     it("Should throw an AppError if it's missing required params", async () => {
         const sut = createBookUseCase({ bookRepository });
         await expect(() => sut({ })).rejects.toThrow(new AppError(AppError.missingParams));
+    });
+
+    it("Should return Either.left if the ISBN's already in use", async () => {
+
+        bookRepository.findByIsbn.mockResolvedValueOnce(true);
+
+        const bookDTO = {
+            name: "valid name",
+            quantity: "valid quantity",
+            author: "valid author",
+            genre: "valid genre",
+            isbn: "INVALID_ISBN",
+        }
+
+        const suv = createBookUseCase({ bookRepository });
+        const output = await suv(bookDTO);
+        expect(output.left).toEqual(Either.FieldAlreadyTaken("isbn"));
+        expect(bookRepository.findByIsbn).toHaveBeenCalledTimes(1);
+        expect(bookRepository.findByIsbn).toHaveBeenCalledWith(bookDTO.isbn)
     })
 
 })
