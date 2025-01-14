a4cec9bf9475273f6b94eda9d56d7e758d2ae533
kellton_hamaia <hamaia.kell@gmail.com>
2025-01-13 21:46:15 -0300

    test: criar teste para devolver livro com atraso e multa

--- src/application/usecases/return-book.js
+++ src/application/usecases/return-book.js
@@ -3,9 +3,11 @@ const { Either } = require("../errors")
 module.exports = function returnBookUseCase ({ loanRepository }) {
     
     return async ({ loanmentId, returnDate }) => {
-        await loanRepository.return({ loanmentId, returnDate });
         
-        const verifyFine = "Fine not applied";
+        const { returnBookDate } = await loanRepository.return({ loanmentId, returnDate });
+        
+        const applyFine = new Date(returnBookDate).getTime() < new Date(returnDate).getTime();
+        const verifyFine = applyFine ? "Fine Applied: $10.00" : "Fine not applied";
         return Either.Right(verifyFine);
     }
 
--- src/application/usecases/return-book.spec.js
+++ src/application/usecases/return-book.spec.js
@@ -5,8 +5,12 @@ describe("ReturnBookUsecase", () => {
     const loanRepository = {
         return: jest.fn()
     }
-
+    
     it("Should be able to return a book without a fine", async () => {
+
+        loanRepository.return.mockResolvedValueOnce({
+            returnBookDate: new Date("2024-09-01"),
+        });
 
         const returnBookDTO = {
             loanmentId: "ANY_ID",
@@ -19,5 +23,27 @@ describe("ReturnBookUsecase", () => {
         expect(output.right).toBe("Fine not applied");
         expect(loanRepository.return).toHaveBeenCalledTimes(1);
         expect(loanRepository.return).toHaveBeenCalledWith(returnBookDTO);
+    });
+
+    it("Should return a book applying a fine to it", async () => {
+        
+        loanRepository.return.mockResolvedValueOnce({
+            returnBookDate: new Date("2024-08-01"),
+        });
+
+        const returnBookDTO = {
+            loanmentId: "ANY_ID",
+            returnDate: new Date("2024-08-11"),
+        }
+
+        const sut = returnBook({ loanRepository });
+        const output = await sut(returnBookDTO);
+
+        expect(output.right).toBe("Fine Applied: $10.00");
+        expect(loanRepository.return).toHaveBeenCalledTimes(1);
+        expect(loanRepository.return).toHaveBeenCalledWith(returnBookDTO);
+
     })
+
+
 })
