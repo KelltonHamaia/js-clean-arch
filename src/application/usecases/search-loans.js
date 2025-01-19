4cf7f094293a6f4f649938fde251636b466a28cb
kellton_hamaia <hamaia.kell@gmail.com>
2025-01-19 00:08:21 -0300

    feat: criar searchLoans usecase

---
+++ src/application/usecases/search-loans.js
@@ -0,0 +1,11 @@
+const { AppError, Either } = require("../errors")
+
+module.exports = function searchPendingLoans({ loanRepository }) {
+    
+    if(!loanRepository) return new AppError(AppError.dependencies);
+
+    return async () => {
+        let loans = await loanRepository.findPendingLoansWithUserAndbook();
+        return Either.Right(loans);
+    }
+}
